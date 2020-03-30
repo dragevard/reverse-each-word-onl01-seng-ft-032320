6fbd514e823cf2c657656c97d084691dfcd02b28
Derek phung <dragevards@gmail.com>
2020-03-30 23:47:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,12 @@ def reverse_each_word(string)
   end
   new_string.join(" ")
 end
+
+def reverse_each_word(string)
+  new_string = []
+  array - string.split(" ")
+  array.collect do |word|
+    new_string.push(word.reverse)
+  end
+  new_string.join(" ")
+end
